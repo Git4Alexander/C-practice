1838b483023ffd28f5c903fceaeddcd52e6890c5
Alexander <alexander.a.afanasyev@gmail.com>
2022-09-21 12:02:28 +0300

    Практика по языкам программирования 1

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите квадрат этого числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int square = numberB * numberB;
+
+if (numberA == square)
+{
+    Console.WriteLine("Верно");
+}
+else
+{
+    Console.WriteLine($"Не верно! Квадрат числа {numberB} = {square} ");
+}
