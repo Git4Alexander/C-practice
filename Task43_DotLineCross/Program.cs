1aac5811cbabb11337515a51a23ff5fc4e697183
Alexander <alexander.a.afanasyev@gmail.com>
2022-10-19 12:54:34 +0300

    Урок и практика 6, C#

--- Task43_DotLineCross/Program.cs
+++ Task43_DotLineCross/Program.cs
@@ -5,3 +5,44 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Задайте точки в последовательности b1, k1, b2 и k2:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double K1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+
+double[] newArray = NewArray (b1, K1, b2, k2);
+Console.WriteLine($"Точка перечечения двух прямых:"); 
+PrintArray(newArray);
+
+
+double [] NewArray (double b11, double k11, double b22, double k22)
+{
+    double x = ((b22-b11)/(k11-k22));
+    double y = k11*x+b11;
+    double [] array = {x, y};
+
+return array;
+}
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+//         else Console.Write($"{array[i]}");
+//     }
+//     Console.WriteLine("]");
+// }
