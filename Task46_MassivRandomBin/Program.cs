9019c1022aca74c4120ad98d6745ef74ee001302
Alexander <alexander.a.afanasyev@gmail.com>
2022-10-19 21:36:43 +0300

    Урок и практика 76, C#

---
+++ Task46_MassivRandomBin/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+
+int[,] CreateArrayRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] matrix)
+{
+    Console.Write("[");
+    for (int i = 0; i < rows; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
