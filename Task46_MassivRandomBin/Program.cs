2475f0b075685d4e1965a34df01508baebdf50a9
Alexander <alexander.a.afanasyev@gmail.com>
2022-10-20 16:14:49 +0300

    Урок и практика 7, C#

--- Task46_MassivRandomBin/Program.cs
+++ Task46_MassivRandomBin/Program.cs
@@ -6,26 +6,34 @@
 // 77 3 8 1
 
 
-int[,] CreateArrayRndInt(int rows, int columns, int min, int max)
+int[,] CreateMartixRndInt(int rows, int columns, int min, int max)
 {
     int[,] matrix = new int[rows, columns];
     var rnd = new Random();
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows (0) = matrix.GetLength(0)
     {
-        for (int j = 0; j < columns; j++)
+        for (int j = 0; j < matrix.GetLength(1); j++) //columns (1) = matrix.GetLength(1)
         {
-            array[i, j] = rnd.Next(min, max + 1);
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
     }
-    return array;
+    return matrix;
 }
 
-void PrintArray(int[,] matrix)
+void PrintMatrix(int[,] matrix)
 {
-    Console.Write("[");
-    for (int i = 0; i < rows; i++)
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
-        else Console.Write($"{array[i]}");
+        Console.Write("|");
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
     }
-    Console.WriteLine("]");
+}
+int[,] array2D = CreateMartixRndInt(3, 4, -100, 100);
+PrintMatrix(array2D);
